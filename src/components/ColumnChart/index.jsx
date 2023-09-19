8ea3b284803ba625f33a0f9063d62f972ab07f51
deinsteins <hidayaatdenii@gmail.com>
2023-09-19 20:58:29 +0700

    fix graph data current month

--- src/components/ColumnChart/index.jsx
+++ src/components/ColumnChart/index.jsx
@@ -1,10 +1,60 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 const ColumnChart = ({monthlyData}) => {
-  console.log(monthlyData[0]?.total_visitor)
   const [data, setData] = useState([
     {
+      name: 'Januari',
+      data: 120
+    },
+    {
+      name: 'Februari',
+      data: 170
+    },
+    {
+      name: 'Maret',
+      data: 210
+    },
+    {
+      name: 'April',
+      data: 153
+    },
+    {
+      name: 'Mei',
+      data: 202
+    },
+    {
+      name: 'Juni',
+      data: 227
+    },
+    {
+      name: 'Juli',
+      data: 255
+    },
+    {
+      name: 'Agustus',
+      data: 311
+    },
+    {
+      name: 'September',
+      data: 0
+    },
+    {
+      name: 'Oktober',
+      data: 0
+    },
+    {
+      name: 'November',
+      data: 0
+    },
+    {
+      name: 'Desember',
+      data: 0
+    }
+  ]);
+
+  useEffect(() => {
+    setData([{
       name: 'Januari',
       data: 120
     },
@@ -51,8 +101,8 @@ const ColumnChart = ({monthlyData}) => {
     {
       name: 'Desember',
       data: 0
-    }
-  ]);
+    }])
+  }, [monthlyData])
 
   return (
       <ResponsiveContainer width='100%' height={400}>
